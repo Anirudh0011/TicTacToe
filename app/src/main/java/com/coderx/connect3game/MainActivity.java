e534f4966991edc8d1f9457cd5797f22514eb2fa
Anirudh0011 <anirudh16june@gmail.com>
2020-04-20 23:55:27 +0530

    Inital Commit

---
+++ app/src/main/java/com/coderx/connect3game/MainActivity.java
@@ -0,0 +1,61 @@
+package com.coderx.connect3game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    //yellow :0   red: 1  empty : 2
+
+    int [] gameState ={2,2,2,2,2,2,2,2,2};
+    int [][] winningPositions ={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+
+     int activePlayer=0;
+
+    public void dropIn(View view)
+    {
+        ImageView counter =(ImageView) view;
+        Log.i("info", counter.getTag().toString());
+
+        int tappedcounter = Integer.parseInt(counter.getTag().toString());
+        gameState[tappedcounter] =activePlayer;
+        counter.setTranslationY(-1500);
+
+        if(activePlayer == 0)
+        {
+
+            counter.setImageResource(R.drawable.yellow);
+
+            activePlayer = 1;
+        }
+        else
+        {
+            counter.setImageResource(R.drawable.red);
+
+            activePlayer = 0;
+
+        }
+
+        counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
+        for(int[] winningPostitions : winningPositions)
+        {
+            if(gameState[winningPostitions[0]] == gameState[winningPostitions[1]] && gameState[winningPostitions[1]] == gameState[winningPostitions[2]] && gameState[winningPostitions[0]] !=2)
+            {
+                // Someone has won!
+
+                Toast.makeText(this, "Someone has won", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
